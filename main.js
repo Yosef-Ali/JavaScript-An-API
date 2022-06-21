079aa4a78477700c15cdc7c061ff3121d240a6d0
apple <apple@apples-MacBook-Pro.local>
2022-06-21 07:09:39 +0300

    fetch data with async function

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log('Hello from main.js');
+// fetch('https://dog.ceo/api/breeds/list/all')
+// 	.then(function (response) {
+// 		return response.json();
+// 	})
+// 	.then(function (data) {
+// 		console.log(data);
+// 	});
+
+async function getDogs() {
+	const response = await fetch('https://dog.ceo/api/breeds/list/all');
+	const data = await response.json();
+	console.log(data);
+}
+
+getDogs();
