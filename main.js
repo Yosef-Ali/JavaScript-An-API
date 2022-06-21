0cd75b42b3e5d3ce331a859bc198054737aa4ec9
apple <apple@apples-MacBook-Pro.local>
2022-06-21 08:07:26 +0300

    create breed list function

--- main.js
+++ main.js
@@ -9,7 +9,19 @@
 async function getDogs() {
 	const response = await fetch('https://dog.ceo/api/breeds/list/all');
 	const data = await response.json();
-	console.log(data);
+	createBreedList(data.message);
 }
 
 getDogs();
+
+function createBreedList(breeds) {
+	const breedList = document.getElementById('breed-list');
+	breedList.innerHTML = `<select>
+		<option value="">Select a breed</option>
+		${Object.keys(breeds)
+			.map(function (breed) {
+				return `<option>${breed}</option>`;
+			})
+			.join('')}
+	</select>`;
+}
